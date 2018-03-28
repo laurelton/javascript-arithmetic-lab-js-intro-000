41bd5374b8dd211a5c6773359741d7bc02b44bc8
Frank Rose <frankrosework@gmail.com>
2018-03-28 23:33:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var add = function(a, b) {
+  return a + b;
+}
+
+var subtract = function(a, b) {
+  return a - b;
+}
+
+var multiply = function(a, b) {
+  return a * b;
+}
+
+var divide = function(a, b) {
+  return a / b;
+}
+
+var inc = function(n) {
+  return ++n;
+}
+
+var dec = function(n) {
+  return --n;
+}
+
